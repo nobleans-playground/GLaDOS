592281bdc9d22de744d0c52aa124464bc66f6ae1
Lichor8 <rikstruijk@protonmail.ch>
2021-09-05 21:05:33 +0200

    Update documentation and created working launch file for cpp_pubsub.

--- doc/ros2_commands.md
+++ doc/ros2_commands.md
@@ -4,6 +4,10 @@ sudo apt install ros-$ROS_DISTRO-webots-ros2
 # Now, if Webots is not installed (or not the right version), when launching the node, a pop-up window will ask the user if he wants to install Webots, if he says yes, the tarball is downloaded and installed in $HOME/.ros/webots$VERSION, I have checked but did not found any recommended location to save such files (some other packages such RVIz2 are creating their own hidden folder in the home directory).
 ros2 launch webots_ros2_demos armed_robots.launch.py
 
+# cheat sheets
+- https://github.com/ubuntu-robotics/ros2_cheats_sheet/blob/master/cli/cli_cheats_sheet.pdf
+- https://gist.github.com/kscottz/ef8a62700ef6ae5e04a0e91af5453ccd
+
 # create
 ros2 pkg create --build-type ament_cmake <package_name>
 ros2 pkg create --build-type ament_python <package_name>
@@ -17,9 +21,16 @@ ros2 run cpp_pubsub listener
 ros2 launch my_package main.launch.py
 
 # debug
+ros2 node list
+ros2 topic list
+ros2 topic <command> <topic>
+ros2 run rqt_gui rqt_gui
+ros2 run rqt_graph rqt_graph
 ros2 run tf2_ros static_transform_publisher 1 2 3 0.5 0.1 -1.0 foo bar
 ros2 run tf2_ros tf2_echo foo bar
 ros2 run tf2_tools view_frames.py
+ros2 interface list
+ros2 doctor
 
 # colcon_cd
 source first?
@@ -29,6 +40,7 @@ colcon_cd
 
 # TODO
 currently working on:
+- ament_cmake (https://docs.ros.org/en/foxy/Guides/Ament-CMake-Documentation.html) or colcon build?
+- pubsub tutorial -> python launch file -> 2 forms of python launch file, check both and understand them (see main.launch.py)
 - box on wheels
-    - ament_cmake (https://docs.ros.org/en/foxy/Guides/Ament-CMake-Documentation.html) or colcon build?
-    - pubsub tutorial -> python launch file -> 2 forms of python launch file, check both and understand them (see main.launch.py)
+
--- src/cpp_pubsub/launch/main.launch.py
+++ src/cpp_pubsub/launch/main.launch.py
@@ -1,45 +1,50 @@
-import launch
-import launch.actions
-import launch.substitutions
-import launch_ros.actions
-
-
-def generate_launch_description():
-    return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(
-            'node_prefix',
-            default_value=[launch.substitutions.EnvironmentVariable('USER'), '_'],
-            description='Prefix for node names'),
-        launch_ros.actions.Node(
-            package='demo_nodes_cpp', executable='talker', output='screen',
-            name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'talker']),
-    ])
-
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='turtlesim',
-            namespace='turtlesim1',
-            executable='turtlesim_node',
-            name='sim'
+            package='cpp_pubsub',
+            namespace='talker_ns',
+            executable='talker',
+            name='talker_name',
+            output={
+                'stdout': 'screen',
+                'stderr': 'screen',
+            },
+            emulate_tty=True
         ),
         Node(
-            package='turtlesim',
-            namespace='turtlesim2',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='turtlesim',
-            executable='mimic',
-            name='mimic',
+            package='cpp_pubsub',
+            namespace='listener_ns',
+            executable='listener',
+            name='listener_name',
             remappings=[
-                ('/input/pose', '/turtlesim1/turtle1/pose'),
-                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-            ]
-        )
+                ('/listener_ns/topic', '/talker_ns/topic'),
+            ],
+            output={
+                'stdout': 'screen',
+                'stderr': 'screen',
+            },
+            emulate_tty=True
+        ),
     ])
+
+
+
+# import launch
+# import launch.actions
+# import launch.substitutions
+# import launch_ros.actions
+
+
+# def generate_launch_description():
+#     return launch.LaunchDescription([
+#         launch.actions.DeclareLaunchArgument(
+#             'node_prefix',
+#             default_value=[launch.substitutions.EnvironmentVariable('USER'), '_'],
+#             description='Prefix for node names'),
+#         launch_ros.actions.Node(
+#             package='demo_nodes_cpp', executable='talker', output='screen',
+#             name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'talker']),
+#     ])
